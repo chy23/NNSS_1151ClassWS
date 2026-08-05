c735a8efe365726e4caf66cbb897a91e380ea250
Chiahua <chiahua@Chiahua-MacBook-Pro-M1-2.local>
2026-08-05 12:57:24 +0800

    隱藏版權宣告：在程式碼中加入建立者資訊

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log("網站建立自楊家驊老師 The website was created by Teacher ChiahuaYang");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
