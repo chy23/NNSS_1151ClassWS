86211f07652c92e608769dca43931230a2777c94
Chiahua <chiahua@Chiahua-MacBook-Pro-M1-2.local>
2026-08-06 10:26:44 +0800

    [115-1-L1] 設定單元範圍 [unit1]

--- src/data/lessons-meta.js
+++ src/data/lessons-meta.js
@@ -3,20 +3,5 @@ export const lessonsMeta = [
     "id": "lesson-1",
     "lessonNum": "第一單元",
     "lessonName": "日治時期臺灣的發展與變遷"
-  },
-  {
-    "id": "lesson-2",
-    "lessonNum": "第二單元",
-    "lessonName": "邁向民主的社會"
-  },
-  {
-    "id": "lesson-3",
-    "lessonNum": "第三單元",
-    "lessonName": "戰後臺灣的經濟與城鄉發展"
-  },
-  {
-    "id": "lesson-4",
-    "lessonNum": "第四單元",
-    "lessonName": "臺灣進行式"
   }
 ];
